1eee820f7eb06f7ee999b38524570d212cd04116
Oliver-08-ojla <oliver2018es@gmail.com>
2023-02-08 21:19:52 -0500

    Migraciones de todas las tablas

---
+++ database/migrations/2023_02_09_015559_create_prestamos_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prestamos', function (Blueprint $table) {
+            $table->id();
+            $table->date('fechaPrestamo');
+            $table->date('fechaDevolucion');
+            $table->date('fechaRealDevolucion');
+            $table->foreignId('libro_id')
+            ->constrained('libros')
+            ->cascadeOnUpdate()
+            ->cascadeOnDelete();
+            $table->foreignId('cliente_id')
+            ->constrained('clientes')
+            ->cascadeOnUpdate()
+            ->cascadeOnDelete();
+            $table->foreignId('usuario_id')
+            ->constrained('usuarios')
+            ->cascadeOnUpdate()
+            ->cascadeOnDelete();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prestamos');
+    }
+};
