00abea60482b4b81775d5d92bae5ab086d2469bf
Oliver-08-ojla <oliver2018es@gmail.com>
2023-02-15 23:30:41 -0500

    finalizacion

--- routes/api.php
+++ routes/api.php
@@ -6,21 +6,12 @@ use App\Http\Controllers\PrestamoController;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::post('/register', [UsuarioController::class, 'register']);
 Route::post('/login', [UsuarioController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group( function () {
+    //aquí ban todas las rutas pero como no está validado en angular por enden están agu
 });
 Route::resource('libros', LibroController::class);
 Route::get('prestamos/libros/lend/{id}', [PrestamoController::class,'lendBook']);
@@ -29,47 +20,4 @@ Route::resource('prestamos', PrestamoController::class);
 Route::get('prestamos/libros/user/{id}', [PrestamoController::class,'bookLend']);
 Route::get('prestamos/libros/admin', [PrestamoController::class,'booksLendAll']);
 
-
-/* Route::group(
-    ['middleware' => ["auth:sanctum"]],
-    function () {
-        Route::controller(UsuarioController::class)->group(function () {
-            Route::get('userProfile', 'userProfile');
-            Route::post('logout', 'logout');
-        });
-        Route::controller(PrestamoController::class)->group(function () {
-            Route::get('prestamos', 'index');
-            Route::get('prestamos/{id}', 'show');
-            Route::post('prestamos', 'store');
-            Route::post('devolver/{id}', 'devolverLibro');
-            Route::get('listado', 'listado');
-            Route::get('listadoUC', 'listadoUC');
-        });
-        
-    }
-
-);
-
-Route::controller(UsuarioController::class)->group(function () {
-    Route::post('register', 'register');
-    Route::post('login', 'login');
-});
-
-
-Route::controller(LibroController::class)->group(function () {
-    Route::get('libros', 'index');
-    Route::get('libros/{id}', 'show');
-    Route::post('libros', 'store');
-    Route::put('libros/{id}', 'update');
-    Route::delete('libros/{id}', 'destroy');
-});
-
-Route::controller(ClienteController::class)->group(function(){
-
-    Route::get('clientes', 'index');
-    Route::get('clientes/{id}', 'show');
-    Route::post('clientes', 'store');
-    Route::post('clientes/{id}', 'update');
-    Route::delete('clientes/{id}', 'destroy');
-}); */
 
